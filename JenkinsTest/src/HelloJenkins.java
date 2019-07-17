a6d6dbbb34fed5553a88ed74e003a52a2e162b57
SagarMD12194 <sagarmd888@gmail.com>
2019-07-17 21:57:45 +0530

    testing

--- JenkinsTest/src/HelloJenkins.java
+++ JenkinsTest/src/HelloJenkins.java
@@ -2,7 +2,9 @@
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-System.out.println("Hello Jenkins programming....webhooks...sagar");
+System.out.println("Hello Jenkins programming....");
+
+System.out.println("Hello sagar");
 	}
 
 }
